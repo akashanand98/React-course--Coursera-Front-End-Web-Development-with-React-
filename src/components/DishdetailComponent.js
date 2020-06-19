d54ff1016507b04b50cf63568d5f7b90cd773cab
Akash Anand <akashbavaanand@gmail.com>
2020-06-19 15:18:16 +0530

    4th week 2nd commit

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,6 +7,10 @@ import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
+
+
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -164,7 +168,7 @@ function RenderDish({dish}) {
        return(
 
            <Card>
-               <CardImg top src={dish.image} alt={dish.name} />
+               <CardImg src={baseUrl + dish.image} alt={dish.name} />
                <CardBody>
                  <CardTitle>{dish.name}</CardTitle>
                  <CardText>{dish.description}</CardText>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardImgOverlay,
         CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 function RenderMenuItem({dish, onClick})
@@ -11,7 +12,7 @@ function RenderMenuItem({dish, onClick})
   return (
            <Card>
                 <Link to={`/menu/${dish.id}`} >
-               <CardImg width="100%" src={dish.image} alt={dish.name} />
+               <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                <CardImgOverlay>
                    <CardTitle>{dish.name}</CardTitle>
                </CardImgOverlay>
@@ -53,7 +54,7 @@ const Menu =(props)=>{
           <div  className="col-12 col-md-5 m-1">
           <RenderMenuItem dish={dish} onClick={props.onClick} />
           </div>
-        
+
 
       );
     });
